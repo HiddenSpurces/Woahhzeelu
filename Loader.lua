5914343705676b546d48a065bfeec588d4574361
HiddenSpurces <sourceshidden@gmail.com>
2026-06-27 21:41:40 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,13 +1,3 @@
 --topbar
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HiddenSpurces/Woahhzeelu/refs/heads/main/games/Backup.lua"))()
-
-wait(1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HiddenSpurces/Woahhzeelu/refs/heads/main/assets/topbar.lua"))()
---anti scam msg
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HiddenSpurces/Woahhzeelu/refs/heads/main/assets/warningmsg.lua"))()
----auto updater
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HiddenSpurces/Woahhzeelu/refs/heads/main/assets/autoupdater.lua"))()
------
-wait(30)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HiddenSpurces/Woahhzeelu/refs/heads/main/assets/discordjoiner.lua"))()
